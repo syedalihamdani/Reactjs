5001e667b8ad18147c35c31614404082e772dce2
Hafiz Sayyed Ali Hamdani <alihamdani61@yahoo.com>
2021-06-14 09:04:25 +0500

    new sliders

--- slider/src/Slider.jsx
+++ slider/src/Slider.jsx
@@ -1,40 +1,32 @@
+ import React, { useState } from 'react';
+ import  './styles.css';
+ import image0 from './images/0.jpg';
+ import image1 from './images/1.jpg';
+ import image2 from './images/2.jpg';
  
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+ 
+   const Slider=()=>{
 
-// Import Swiper styles
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css"
-import "swiper/components/navigation/navigation.min.css"
+   return (
+     <>
+       <div className="container">
+  <div className="wrapper">
+    <div className="slider-holder">
+      <div id="slider_image_1"><img src={image0} alt='1' /> </div>
+      <div id="slider_image_2"><img src={image1} alt='1'/></div>
+      <div id="slider_image_3"><img src={image2} alt='1' /></div>
+    </div>
+  </div>
+  <div className="button-holder">
+    <a href="#slider_image_1" className="dots" />
+    <a href="#slider_image_2" className="dots" />
+    <a href="#slider_image_3" className="dots" />
+  </div>
+</div>
 
-import "./styles.css";
-
-
-// import Swiper core and required modules
-import SwiperCore, {
-  Autoplay,Pagination,Navigation
-} from 'swiper/core';
-
-// install Swiper modules
-SwiperCore.use([Autoplay,Pagination,Navigation]);
-
-
-export default function App() {
-  
-  
-  
-  return (
-    <>
-    <Swiper spaceBetween={30} centeredSlides={true} autoplay={{
-  "delay": 2500,
-  "disableOnInteraction": false
-}} pagination={{
-  "clickable": true
-}} navigation={true} className="mySwiper">
-  <SwiperSlide><img src="https://source.unsplash.com/user/erondu/1600x900" alt='df' />
-</SwiperSlide><SwiperSlide>2</SwiperSlide><SwiperSlide>Slide 3</SwiperSlide><SwiperSlide>Slide 4</SwiperSlide><SwiperSlide>Slide 5</SwiperSlide><SwiperSlide>Slide 6</SwiperSlide><SwiperSlide>Slide 7</SwiperSlide><SwiperSlide>Slide 8</SwiperSlide><SwiperSlide>Slide 9</SwiperSlide>
-  </Swiper>
-    </>
-  )
-}
+     </>
+   )
+ }
+ 
+ export default Slider;
+ 
